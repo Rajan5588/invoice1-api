eaa723b669e8424c0a67fe28f71afa18a845604d
Rajan5588 <rajankumarg301@gmail.com>
2026-07-06 12:11:23 +0530

    Added subscription columns to users table

---
+++ database/migrations/2026_07_06_064022_add_subscription_columns_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('subs_id')->nullable()->after('otp_verified');
+
+            $table->dateTime('subs_expired_date')->nullable()->after('subs_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->dropColumn([
+                'subs_id',
+                'subs_expired_date'
+            ]);
+
+        });
+    }
+};
